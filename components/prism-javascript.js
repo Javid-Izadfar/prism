3c4864f923bdde7f3cac880ae5878b09b72e2c02
Javid Izadfar <javid.izadfar@gmail.com>
2016-10-27 23:56:01 +0330

    minor bug fixes
    changed some of keyword types

--- components/prism-javascript.js
+++ components/prism-javascript.js
@@ -1,8 +1,8 @@
 Prism.languages.javascript = Prism.languages.extend('clike', {
-	'keyword control': /\b(await|break|case|catch|package|return|switch|throw|try|while|with|yield)\b/,
-	'keyword type': /\b(async|class|const|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|interface|let|new|null|private|protected|public|return|set|static|super|switch|throw|try|typeof|var|void|while|with|yield)\b/,
-	'keyword operator': /\b(as|get|in|instanceof|of|set|typeof|void)\b/,
-	'keyword variable': /\b(super|this)\b/,
+	'keyword control': /\b(await|case|package|switch|with|yield|debugger|continue|default|export|from|import|do|else|for|if|return|throw|try|while)\b/,
+	'keyword type': /\b(async|function|class|const|enum|extends|implements|interface|private|protected|public|static|let|var|length)\b/,
+	'keyword operator': /\b(as|get|delete|of|set|typeof|void)\b/,
+	'keyword variable': /\b(super|this|array)\b/,
 	'number': /\b-?(0x[\dA-Fa-f]+|0b[01]+|0o[0-7]+|\d*\.?\d+([Ee][+-]?\d+)?|NaN|Infinity)\b/,
 	// Allow for all non-ASCII characters (See http://stackoverflow.com/a/2008444)
 	'function': /[_$a-zA-Z\xA0-\uFFFF][_$a-zA-Z0-9\xA0-\uFFFF]*(?=\()/i,
